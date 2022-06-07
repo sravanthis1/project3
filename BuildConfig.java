102c77bad09e2e7d7fca073404659f9c8be82070
sravanthis1 <106096680+sravanthis1@users.noreply.github.com>
2022-06-07 19:12:42 -0400

    Add files via upload

---
+++ BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.gpa_yournetid_calculator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.gpa_yournetid_calculator";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
